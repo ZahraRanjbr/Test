096248190a0bb924e35e5262778a628c07a496ff
Zahra <newemail@example.com>
2024-08-30 17:00:51 +0430

    add guys

--- src/main/java/ir/portal/sso/login/test.java
+++ src/main/java/ir/portal/sso/login/test.java
@@ -5,5 +5,7 @@ public class test {
         System.out.println("Hello Word");
         System.out.println("Change");
         System.out.println("neda");
+        System.out.println("zahra");
+        System.out.println("hello guys");
     }
 }
