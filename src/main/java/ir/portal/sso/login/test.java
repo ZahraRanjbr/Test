1c63a904390fd8adef48ffbd349f2ea4d379b8a7
Zahra <newemail@example.com>
2024-08-30 12:57:05 +0430

    add details

--- src/main/java/ir/portal/sso/login/test.java
+++ src/main/java/ir/portal/sso/login/test.java
@@ -3,5 +3,6 @@ package ir.portal.sso.login;
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello Word");
+        System.out.println("Change");
     }
 }
